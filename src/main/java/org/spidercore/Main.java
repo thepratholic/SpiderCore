6a5b9697c1e4f00521d4b2379247e5e9b4e8adad
thepratholic <chelaramanipratham@gmail.com>
2026-03-26 02:00:40 +0530

    changed

--- src/main/java/org/spidercore/Main.java
+++ src/main/java/org/spidercore/Main.java
@@ -1,17 +1,18 @@
 package org.spidercore;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+ * Main — Ye project ka actual entry point hai
+ *
+ * Abhi WebCrawler.java mein main() method hai —
+ * hum wahan se hi crawler start karenge
+ *
+ * Ye class simply WebCrawler ka main() call karegi
+ * Separation of concerns —
+ * Main = sirf start karo
+ * WebCrawler = actual logic
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WebCrawler.main(args);
     }
 }
